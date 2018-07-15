b40fa5f34d0a32ea1982ae0576455f7fd474b88e
David Widmann <dev+github@devmotion.de>
2018-07-15 13:08:51 +0200

    Use MacroTools and handle subtractions

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 [![codecov.io](http://codecov.io/github/JuliaDiffEq/MuladdMacro.jl/coverage.svg?branch=master)](http://codecov.io/github/JuliaDiffEq/MuladdMacro.jl?branch=master)
 
 This package provides the `@muladd` macro. It automatically converts expressions
-with multiplications and additions to calls with `muladd` which then fuse via
+with multiplications and additions or subtractions to calls with `muladd` which then fuse via
 FMA when it would increase the performance of the code. The `@muladd` macro
 can be placed on code blocks and it will automatically find the appropriate
 expressions and nest muladd expressions when necessary. In mixed expressions summands without multiplication will be grouped together and evaluated first but otherwise the order of evaluation of multiplications and additions is not changed.
@@ -14,15 +14,16 @@ expressions and nest muladd expressions when necessary. In mixed expressions sum
 ## Examples
 
 ```julia
-julia> macroexpand(:(@muladd k3 = f(t + c3*dt, @. uprev+dt*(a031*k1+a032*k2))))
-:(k3 = f((muladd)(c3, dt, t), (muladd).(dt, (muladd).(a032, k2, *.(a031, k1)), uprev)))
-julia> macroexpand(:(@muladd integrator.EEst = integrator.opts.internalnorm((update - dt*(bhat1*k1 + bhat4*k4 + bhat5*k5 + bhat6*k6 + bhat7*k7 + bhat10*k10))./ @. (integrator.opts.abstol+max(abs(uprev),abs(u))*integrator.opts.reltol))))
-:(integrator.EEst = integrator.opts.internalnorm((update - dt * (muladd)(bhat10, k10, (muladd)(bhat7, k7, (muladd)(bhat6, k6, (muladd)(bhat5, k5, (muladd)(bhat4, k4, bhat1 * k1)))))) ./ (muladd).(max.(abs.(uprev), abs.(u)), integrator.opts.reltol, integrator.opts.abstol)))
+julia> @macroexpand(@muladd k3 = f(t + c3*dt, @. uprev+dt*(a031*k1+a032*k2)))
+:(k3 = f((muladd)(c3, dt, t), (muladd).(dt, (muladd).(a032, k2, (*).(a031, k1)), uprev)))
+
+julia> @macroexpand(@muladd integrator.EEst = integrator.opts.internalnorm((update - dt*(bhat1*k1 + bhat4*k4 + bhat5*k5 + bhat6*k6 + bhat7*k7 + bhat10*k10))./ @. (integrator.opts.abstol+max(abs(uprev),abs(u))*integrator.opts.reltol)))
+:(integrator.EEst = (integrator.opts).internalnorm((muladd)(-dt, (muladd)(bhat10, k10, (muladd)(bhat7, k7, (muladd)(bhat6, k6, (muladd)(bhat5, k5, (muladd)(bhat4, k4, bhat1 * k1))))), update) ./ (muladd).(max.(abs.(uprev), abs.(u)), (integrator.opts).reltol, (integrator.opts).abstol)))
 ```
 
 ## Broadcasting
 
-A `muladd` call will be broadcasted if both the `*` and the `+` are broadcasted.
+A `muladd` call will be broadcasted if both the `*` and the `+` or `-` are broadcasted.
 If either one is not broadcasted, then the expression will be converted to a
 non-dotted `muladd`.
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,49 +1,90 @@
-using MuladdMacro
-using Base.Test
+using MuladdMacro, Test
 
 # Basic expressions
 @testset "Basic expressions" begin
-    @test @macroexpand(@muladd a*b+c) == :($(Base.muladd)(a, b, c))
-    @test @macroexpand(@muladd c+a*b) == :($(Base.muladd)(a, b, c))
-    @test @macroexpand(@muladd b*a+c) == :($(Base.muladd)(b, a, c))
-    @test @macroexpand(@muladd c+b*a) == :($(Base.muladd)(b, a, c))
+    @testset "Summation" begin
+        @test @macroexpand(@muladd a*b+c) == :($(Base.muladd)(a, b, c))
+        @test @macroexpand(@muladd c+a*b) == :($(Base.muladd)(a, b, c))
+        @test @macroexpand(@muladd b*a+c) == :($(Base.muladd)(b, a, c))
+        @test @macroexpand(@muladd c+b*a) == :($(Base.muladd)(b, a, c))
+    end
+
+    @testset "Subtraction" begin
+        @test @macroexpand(@muladd a*b-c) == :($(Base.muladd)(a, b, -c))
+        @test @macroexpand(@muladd a-b*c) == :($(Base.muladd)(-b, c, a))
+        @test @macroexpand(@muladd b*a-c) == :($(Base.muladd)(b, a, -c))
+        @test @macroexpand(@muladd a-c*b) == :($(Base.muladd)(-c, b, a))
+    end
 end
 
 # Additional factors
 @testset "Additional factors" begin
-    @test @macroexpand(@muladd a*b*c+d) == :($(Base.muladd)(a*b, c, d))
-    @test @macroexpand(@muladd a*b*c*d+e) == :($(Base.muladd)(a*b*c, d, e))
+    @testset "Summation" begin
+        @test @macroexpand(@muladd a*b*c+d) == :($(Base.muladd)(a*b, c, d))
+        @test @macroexpand(@muladd a*b*c*d+e) == :($(Base.muladd)(a*b*c, d, e))
+    end
+
+    @testset "Subtraction" begin
+        @test @macroexpand(@muladd a*b*c-d) == :($(Base.muladd)(a*b, c, -d))
+        @test @macroexpand(@muladd a*b*c*d-e) == :($(Base.muladd)(a*b*c, d, -e))
+        @test @macroexpand(@muladd a-b*c*d) == :($(Base.muladd)(-(b*c), d, a))
+        @test @macroexpand(@muladd a-b*c*d*e) == :($(Base.muladd)(-(b*c*d), e, a))
+    end
 end
 
 # Multiple multiplications
 @testset "Multiple multiplications" begin
-    @test @macroexpand(@muladd a*b+c*d) == :($(Base.muladd)(c, d, a*b))
-    @test @macroexpand(@muladd a*b+c*d+e*f) == :($(Base.muladd)(e, f,
-                                                                $(Base.muladd)(c, d, a*b)))
-    @test @macroexpand(@muladd a*(b*c+d)+e) == :($(Base.muladd)(a,
-                                                                $(Base.muladd)(b, c, d), e))
+    @testset "Summation" begin
+        @test @macroexpand(@muladd a*b+c*d) == :($(Base.muladd)(c, d, a*b))
+        @test @macroexpand(@muladd a*b+c*d+e*f) ==
+            :($(Base.muladd)(e, f, $(Base.muladd)(c, d, a*b)))
+        @test @macroexpand(@muladd a*(b*c+d)+e) ==
+            :($(Base.muladd)(a, $(Base.muladd)(b, c, d), e))
+    end
+
+    @testset "Subtraction" begin
+        @test @macroexpand(@muladd a*b-c*d) == :($(Base.muladd)(-c, d, a*b))
+        @test @macroexpand(@muladd a*(b*c-d)-e) ==
+            :($(Base.muladd)(a, $(Base.muladd)(b, c, -d), -e))
+    end
 end
 
 # Dot calls
 @testset "Dot calls" begin
-    @test @macroexpand(@. @muladd a*b+c) == :($(Base.muladd).(a, b, c))
-    @test @macroexpand(@muladd @. a*b+c) == :($(Base.muladd).(a, b, c))
-    @test @macroexpand(@muladd a.*b+c) == :(a.*b+c)
-    @test @macroexpand(@muladd a*b.+c) == :(a*b.+c)
-    @test @macroexpand(@muladd .+(a.*b, c, d)) == :($(Base.muladd).(a, b, c.+d))
-    @test @macroexpand(@muladd @. a*b+c+d) == :($(Base.muladd).(a, b, (+).(c, d)))
-    @test @macroexpand(@muladd @. a*b*c+d) == :($(Base.muladd).((*).(a, b), c, d))
-    @test @macroexpand(@muladd f.(a)*b+c) == :($(Base.muladd)(f.(a), b, c))
-    @test @macroexpand(@muladd a*f.(b)+c) == :($(Base.muladd)(a, f.(b), c))
-    @test @macroexpand(@muladd a*b+f.(c)) == :($(Base.muladd)(a, b, f.(c)))
+    @testset "Summation" begin
+        @test @macroexpand(@. @muladd a*b+c) == :($(Base.muladd).(a, b, c))
+        @test @macroexpand(@muladd @. a*b+c) == :($(Base.muladd).(a, b, c))
+        @test @macroexpand(@muladd a.*b+c) == :(a.*b+c)
+        @test @macroexpand(@muladd a*b.+c) == :(a*b.+c)
+
+        @test @macroexpand(@muladd .+(a.*b, c, d)) == :($(Base.muladd).(a, b, c.+d))
+        @test @macroexpand(@muladd @. a*b+c+d) == :($(Base.muladd).(a, b, (+).(c, d)))
+        @test @macroexpand(@muladd @. a*b*c+d) == :($(Base.muladd).((*).(a, b), c, d))
+
+        @test @macroexpand(@muladd f.(a)*b+c) == :($(Base.muladd)(f.(a), b, c))
+        @test @macroexpand(@muladd a*f.(b)+c) == :($(Base.muladd)(a, f.(b), c))
+        @test @macroexpand(@muladd a*b+f.(c)) == :($(Base.muladd)(a, b, f.(c)))
+    end
+
+    @testset "Subtraction" begin
+        @test @macroexpand(@. @muladd a*b-c) == :($(Base.muladd).(a, b, -c))
+        @test @macroexpand(@muladd @. a*b-c) == :($(Base.muladd).(a, b, (-).(c)))
+        @test @macroexpand(@muladd a.*b-c) == :(a.*b-c)
+        @test @macroexpand(@muladd a*b.-c) == :(a*b.-c)
+
+        @test @macroexpand(@. @muladd a-b*c) == :($(Base.muladd).(-b, c, a))
+        @test @macroexpand(@muladd @. a-b*c) == :($(Base.muladd).((-).(b), c, a))
+        @test @macroexpand(@muladd a-b.*c) == :(a-b.*c)
+        @test @macroexpand(@muladd a.-b*c) == :(a.-b*c)
+    end
 end
 
 # Nested expressions
 @testset "Nested expressions" begin
-    @test @macroexpand(@muladd f(x, y, z) = x*y+z) == :(f(x, y, z) =
-                                                        $(Base.muladd)(x, y, z))
+    @test @macroexpand(@muladd f(x, y, z) = x*y+z) ==
+       :(f(x, y, z) = $(Base.muladd)(x, y, z))
     @test @macroexpand(@muladd function f(x, y, z) x*y+z end) ==
         :(function f(x, y, z) $(Base.muladd)(x, y, z) end)
-    @test @macroexpand(@muladd for i in 1:n z = x*i + y end) ==
+    @test @macroexpand(@muladd(for i in 1:n z = x*i + y end)) ==
         :(for i in 1:n z = $(Base.muladd)(x, i, y) end)
 end
