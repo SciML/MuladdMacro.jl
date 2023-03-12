ce5324bd0ffaa77dfce470dac92357449d30eca5
Hendrik Ranocha <ranocha@users.noreply.github.com>
2023-03-13 00:24:16 +0100

    enable dependabot for GitHub actions (#37)
    * enable dependabot for GitHub actions
    
    This allows us to get updates for GitHub actions automatically. I have used this for my own packages as well as our [Trixi.jl framework](https://github.com/trixi-framework). After merging this, we should also enable other Dependabot actions in "Settings -> Code security and analysis -> Dependabot alerts" and "... -> Dependabot security updates".
    
    After merging this, Dependabot will prepare PRs with updates of the versions of GitHub actions we use such as.
    
    I think it would be nice to have this in all of SciML. To get the discussion started, I picked this repo since the CI tests are not very expensive.
    
    * format

--- docs/make.jl
+++ docs/make.jl
@@ -7,8 +7,10 @@ end
 
 using MuladdMacro
 
-cp(joinpath(@__DIR__, "Manifest.toml"), joinpath(@__DIR__, "src/assets/Manifest.toml"); force = true)
-cp(joinpath(@__DIR__, "Project.toml"), joinpath(@__DIR__, "src/assets/Project.toml"); force = true)
+cp(joinpath(@__DIR__, "Manifest.toml"), joinpath(@__DIR__, "src/assets/Manifest.toml");
+   force = true)
+cp(joinpath(@__DIR__, "Project.toml"), joinpath(@__DIR__, "src/assets/Project.toml");
+   force = true)
 
 makedocs(;
          modules = [MuladdMacro],
