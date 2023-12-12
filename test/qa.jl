cdd7a630458148fd2a3b542022a87c9499c5cfcc
Arno Strouwen <arno.strouwen@telenet.be>
2023-12-12 16:29:17 +0100

    test_all Aqua
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/qa.jl
+++ test/qa.jl
@@ -1,11 +1,4 @@
 using MuladdMacro, Aqua
 @testset "Aqua" begin
-    Aqua.find_persistent_tasks_deps(MuladdMacro)
-    Aqua.test_ambiguities(MuladdMacro, recursive = false)
-    Aqua.test_deps_compat(MuladdMacro)
-    Aqua.test_piracies(MuladdMacro)
-    Aqua.test_project_extras(MuladdMacro)
-    Aqua.test_stale_deps(MuladdMacro)
-    Aqua.test_unbound_args(MuladdMacro)
-    Aqua.test_undefined_exports(MuladdMacro)
+    Aqua.test_all(MuladdMacro; test_ambiguities=(recursive = false,))
 end
