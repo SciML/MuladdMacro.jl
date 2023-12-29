bf8aeb36bbfb3d568794ee4fae5a29677ec0f0b0
Arno Strouwen <arno.strouwen@telenet.be>
2023-12-29 15:27:42 +0100

    Julia 1.10 requires remove_linenums! for Expr equality

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,18 +92,16 @@ end
 
 # Nested expressions
 @testset "Nested expressions" begin
-    @test @macroexpand(@muladd f(x, y, z) = x * y + z) ==
-          :(f(x, y, z) = $(Base.muladd)(x, y, z))
-    @test @macroexpand(@muladd function f(x, y, z)
-        x * y + z
-    end) ==
-          :(function f(x, y, z)
-        $(Base.muladd)(x, y, z)
-    end)
-    @test @macroexpand(@muladd(for i in 1:n
-        z = x * i + y
-    end)) ==
-          :(for i in 1:n
-        z = $(Base.muladd)(x, i, y)
-    end)
+    @test Base.remove_linenums!(@macroexpand(@muladd f(x, y, z) = x * y + z)) ==
+          Base.remove_linenums!(:(f(x, y, z) = $(Base.muladd)(x, y, z)))
+    @test Base.remove_linenums!(@macroexpand(
+            @muladd function f(x, y, z)
+                x * y + z
+            end)) ==
+            Base.remove_linenums!(:(
+            function f(x, y, z)
+                $(Base.muladd)(x, y, z)
+            end))
+    @test Base.remove_linenums!(@macroexpand(@muladd(for i in 1:n z = x * i + y end))) ==
+          Base.remove_linenums!(:(for i in 1:n z = $(Base.muladd)(x, i, y) end))
 end
